6da9b086f9906620a93da0c7db222a31a8a80b7d
Anuruddha <anuruddha.rajapaksha@gmail.com>
2022-10-18 13:07:38 +1300

    Add existing project files to Git

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const InputElement = () => {
+
+    const [inputText, setInputText] = useState("");
+    const [historyList, setHistoryList] = useState([]);
+
+    return <div><input
+        onChange={(e) => {
+            setInputText(e.target.value);
+            setHistoryList([...historyList, e.target.value]);
+        }}
+        placeholder="Enter some text" />
+        <br />
+        {inputText}
+        <hr />
+        <br />
+        <ul>
+            {historyList.map((item) => { return <div> {item} </div>; })}
+        </ul>
+    </div>
+
+};
+
+export default InputElement;
